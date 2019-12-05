fa1f0e7662dd8f362ccb2ed303fc4a82d76052aa
Yicong Huang <yicongh1@uci.edu>
2019-12-05 07:35:33 -0800

    create cache dir if not exist

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 import logging
+import os
 
-from paths import KEYWORDS_PATH
+from paths import KEYWORDS_PATH, CACHE_DIR
 
 logging.basicConfig(level=logging.INFO,
                     format='%(asctime)s [%(levelname)s] %(threadName)s:(%(name)s) [%(funcName)s()]   %(message)s')
@@ -8,8 +9,10 @@ logger = logging.getLogger(__name__)
 
 from twi_spider import TwiSpider
 
-if __name__ == '__main__':
+if __name__ == '__main__':        
     logger.setLevel(logging.DEBUG)
+    if not os.path.exists(CACHE_DIR):
+        os.makedirs(CACHE_DIR)
     with open(KEYWORDS_PATH) as file:
         keywords = [line.rstrip() for line in file]
 
