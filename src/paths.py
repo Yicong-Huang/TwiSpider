4fb93cac0f48c3d06319b9b69eedefd1abeb5c93
Yicong Huang <yicongh1@uci.edu>
2019-12-05 07:07:31 -0800

    remove unnecessary paths

--- src/paths.py
+++ src/paths.py
@@ -3,10 +3,7 @@ import os
 # root path of the project
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
 
-# directory for all runnable tasks
-# the task directory should not be changed, otherwise the task manager will crash
-TASK_DIR = os.path.join(ROOT_DIR, 'backend', 'task')
-# directory for all tasks' logs
+# directory for logs
 LOG_DIR = os.path.join(ROOT_DIR, 'backend', 'logs')
 
 # dir for all configs
@@ -16,12 +13,10 @@ CONFIGS_DIR = os.path.join(ROOT_DIR, 'configs')
 DATABASE_CONFIG_PATH = os.path.join(CONFIGS_DIR, 'database.ini')
 # path of the config file for twitter api
 TWITTER_API_CONFIG_PATH = os.path.join(CONFIGS_DIR, 'twitter.ini')
-# path of the config file for logging in task manager
-LOG_CONFIG_PATH = os.path.join(CONFIGS_DIR, 'logger-conf.json')
 
 # dir for all cache
 CACHE_DIR = os.path.join(ROOT_DIR, 'cache')
-
+# path of the job frontier set
 FRONTIER_CACHE = os.path.join(CACHE_DIR, 'twitter.frontier.pickle')
-
+# path of the id CacheSet
 ID_CACHE = os.path.join(CACHE_DIR, 'ids.pickle')
